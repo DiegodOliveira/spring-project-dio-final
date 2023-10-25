eb3ed687fb4bda947715e91e1fd08c6a5c7d4b5f
DiegodOliveira <ciprianodiegooliveira@gmail.com>
2023-10-25 10:49:31 -0300

    third lesson :white_check_mark:

--- credit-application-system/src/main/kotlin/myself/dio/creditapplicationsystem/entity/Customer.kt
+++ credit-application-system/src/main/kotlin/myself/dio/creditapplicationsystem/entity/Customer.kt
@@ -1,12 +1,18 @@
 package myself.dio.creditapplicationsystem.entity
 
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "Cliente")
 data class Customer(
-    var firstName: String = "",
-    var lastName: String = "",
-    val cpf: String = "",
-    var email: String = "",
-    var  password: String = "",
-    var address: Address = Address(),
+    @Column(nullable = false) var firstName: String = "",
+    @Column(nullable = false, unique = true) var lastName: String = "",
+    @Column(nullable = false, unique = true) val cpf: String = "",
+    @Column(nullable = false) var email: String = "",
+    @Column(nullable = false) var  password: String = "",
+    @Column(nullable = false) @Embedded var address: Address = Address(),
+    @Column(nullable = false) @OneToMany(fetch = FetchType.LAZY, cascade = arrayOf(CascadeType.REMOVE,
+        CascadeType.PERSIST), mappedBy = "customer")
     var credits: List<Credit> = mutableListOf(),
-    var id: Long? = null
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null
 )
